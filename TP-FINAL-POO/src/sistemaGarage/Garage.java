3df61a6623b62c3f1d38fa7e09b73cb444c57f24
JuaniPardo <juani.pardo@icloud.com>
2023-06-20 14:57:20 -0300

    Se agregó cola circular para lista de espera de vehículos por atender

--- TP-FINAL-POO/src/sistemaGarage/Garage.java
+++ TP-FINAL-POO/src/sistemaGarage/Garage.java
@@ -1,9 +1,38 @@
 package sistemaGarage;
 
+import vehiculos.Vehiculo;
+
+import java.util.ArrayList;
+
 public class Garage {
     private int capacidadMax;
     private double  precioCubierta;
+    private Cola colaAtencion;
+    private ArrayList<Vehiculo> listaAtendidos;
+
+    //CONSTRUCTOR
+    public Garage(int capacidadMax, double precioCubierta) {
+        this.capacidadMax = capacidadMax;
+        this.precioCubierta = precioCubierta;
+        colaAtencion = new Cola(capacidadMax);
+        listaAtendidos = new ArrayList<>();
+    }
 
     //Metodos
+    public void ingresarVehiculo(Vehiculo v) {
+        colaAtencion.meter(v);
+    }
+
+    public void procesarVehiculo() {
+        Vehiculo v;
+        v = colaAtencion.sacar();
+        if (v != null) {
+            listaAtendidos.add(v);
+        }
+    }
+
+    public void mostrarAtendidos() {
+        listaAtendidos.forEach(System.out::println);
+    }
 
 }
